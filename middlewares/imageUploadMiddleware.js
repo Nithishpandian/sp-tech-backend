7070a0ef9a59e7fa1b2dbcc971da432bbd18c729
Nithishpandian <nithishpandiyan75@gmail.com>
2023-12-27 01:44:51 +0530

    change in image middleware

--- middlewares/imageUploadMiddleware.js
+++ middlewares/imageUploadMiddleware.js
@@ -1,20 +1,26 @@
 const multer = require("multer");
-let path = require("path");
+const path = require("path");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "images/");
+    const destinationPath = "images/";
+
+    // Create the destination directory if it doesn't exist
+    if (!fs.existsSync(destinationPath)) {
+      fs.mkdirSync(destinationPath, { recursive: true });
+    }
+    cb(null, destinationPath);
   },
   filename: function (req, file, cb) {
-    const filename = Date.now() + path.extname(file.originalname)
-    req.filename = filename
+    const filename = Date.now() + path.extname(file.originalname);
+    req.filename = filename;
     cb(null, filename);
   },
 });
 
-
 const upload = multer({ storage: storage });
 
 module.exports = {
-  upload
-}
+  upload,
+};
