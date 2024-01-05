ce4a0cbd77505c848975f83d812082410fd2a1c1
Nithishpandian <nithishpandiyan75@gmail.com>
2024-01-05 11:22:44 +0530

    added edit option

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -2,12 +2,24 @@ const Product = require("../models/ProductModel");
 const cloudinary = require("../config/cloudinary");
 
 // @desc    get products
-// @route   GET /api/getproducts
+// @route   GET /api/product/getproducts
 // @access  private
 const getProduct = async (req, res) => {
   try {
     const products = await Product.find();
     res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// @desc    get selected product
+// @route   GET /api/product/:id
+// @access  private
+const getSelectedProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -37,15 +49,40 @@ const setProduct = async (req, res) => {
   }
 };
 
+// @desc    update products
+// @route   POST /api/product/updateproduct/:id
+// @access  private
+const updateProduct = async (req, res) => {
+  try {
+    const { productName, productDescription } = req.body;
+    const result = await cloudinary.uploader.upload(req.body.productImage, {
+      folder: "uploads",
+    });
+    const product = await Product.findById(req.params.id);
+    product.productName = productName;
+    product.productDescription = productDescription;
+    product.productImage = {
+      public_id: result.public_id,
+      url: result.secure_url,
+    };
+    await product.save();
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// @desc    delete products
+// @route   POST /api/product/deleteproduct
+// @access  private
 const deleteProduct = async (req, res) => {
   const { id } = req.body;
   try {
     const deletedProduct = await Product.findByIdAndDelete(id);
-    console.log(deletedProduct);
     res.status(200).json({ message: "Product deleted successfully" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-module.exports = { getProduct, setProduct, deleteProduct };
+module.exports = { getProduct, getSelectedProduct, setProduct, updateProduct, deleteProduct };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const { getProduct, setProduct, deleteProduct } = require("../controllers/productControllers")
+const { getProduct, getSelectedProduct, setProduct, updateProduct, deleteProduct } = require("../controllers/productControllers")
 
 const { protect } = require("../middlewares/authMiddleware")
 
 router.get("/getproducts", getProduct)
+router.get("/:id", getSelectedProduct)
 router.post("/", setProduct)
+router.post("/updateproduct/:id", updateProduct)
 router.post("/deleteproduct", deleteProduct)
 
 module.exports = router
